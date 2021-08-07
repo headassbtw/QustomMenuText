ba28c7d79b414befade9909a8cd713ac43c4be35
headassbtw <cooldood909@gmail.com>
2021-08-06 20:31:00 -0700

    ingame text editing woo

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -6,8 +6,8 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
     $buildScript += ".cmd"
 }
 
-$ArchiveName = "QustomMenuText_v0.1.0.qmod"
-$TempArchiveName = "QustomMenuText_v0.1.0.qmod.zip"
+$ArchiveName = "QustomMenuText_v0.1.5.qmod"
+$TempArchiveName = "QustomMenuText_v0.1.5.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
 Compress-Archive -Path "./libs/arm64-v8a/libQustomMenuText.so", "./libs/arm64-v8a/libbeatsaber-hook_2_2_4.so", "./libs/arm64-v8a/libquestui.so", "./libs/arm64-v8a/libbs-utils.so", "./mod.json", "./NeonTubes2" -DestinationPath $TempArchiveName -Force
