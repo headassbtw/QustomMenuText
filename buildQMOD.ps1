85d2fc4754187b90652e555aaecb177daa7bafc8
headassbtw <cooldood909@gmail.com>
2021-08-16 18:36:36 -0700

    version bumperoni

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -6,9 +6,9 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
     $buildScript += ".cmd"
 }
 
-$ArchiveName = "QustomMenuText_v0.1.6.qmod"
-$TempArchiveName = "QustomMenuText_v0.1.6.qmod.zip"
+$ArchiveName = "QustomMenuText_v0.1.8.qmod"
+$TempArchiveName = "QustomMenuText_v0.1.8.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/libQustomMenuText.so", "./libs/arm64-v8a/libbeatsaber-hook_2_2_4.so", "./libs/arm64-v8a/libquestui.so", "./libs/arm64-v8a/libbs-utils.so", "./mod.json", "./NeonTubes2" -DestinationPath $TempArchiveName -Force
+Compress-Archive -Path "./libs/arm64-v8a/libQustomMenuText.so", "./libs/arm64-v8a/libbeatsaber-hook_2_2_5.so", "./libs/arm64-v8a/libquestui.so", "./libs/arm64-v8a/libbs-utils.so", "./mod.json", "./NeonTubes2" -DestinationPath $TempArchiveName -Force
 Move-Item $TempArchiveName $ArchiveName -Force
